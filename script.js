c752f1c7bd111826e9bb0191e850219dae7a773e
Глеб <119003629+glebchik1995@users.noreply.github.com>
2023-05-30 21:47:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 document.body.innerHTML='<h1>Hello Glebs from JavaScript </h1>'
 document.body.innerHTML='<h1>Hello Glebss from JavaScript </h1>'
+document.body.innerHTML='<h1>Hello Glebssss from JavaScript </h1>'
+
